4e6c4d388dbfe787542ac453b61b193be932c23e
Adrian Danindra <60964359+Bluejake3@users.noreply.github.com>
2020-12-17 17:02:15 +0700

    Update log.php

--- log.php
+++ log.php
@@ -6,7 +6,7 @@ $query=mysqli_query($koneksi, "select * from log");
 	<table border="1">
 	<tr>
 	<th>Nomor</th><th>Nama</th>
-	<th>Alamat</th>
+	<th>Waktu Akses</th>
 	</tr>
 	<?php
 	while($row=mysqli_fetch_array($query)){
